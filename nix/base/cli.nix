37ef0ff19bf6a27579e3082156a57711f5b7314d
Arne Stulken <21034491+arstulke@users.noreply.github.com>
2024-12-08 23:51:28 +0100

    worked

--- nix/base/cli.nix
+++ nix/base/cli.nix
@@ -3,7 +3,7 @@
 {
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.11"; # Did you read the comment?
 
   # self-explaining one-liners
   console.keyMap = "de";
