3e6c643958845b807ec4f3ab9a34480c901002e9
Arne Stulken <21034491+arstulke@users.noreply.github.com>
2025-11-19 21:20:47 +0100

    virtual-remote-screen: improving retry behaviour of usbip service

--- nix/base/modules/virtual-remote-screen.nix
+++ nix/base/modules/virtual-remote-screen.nix
@@ -58,6 +58,10 @@
         ${pkgs.bash}/bin/bash -c '${pkgs.linuxPackages.usbip}/bin/usbip attach --remote 192.168.0.21 --busid $(${pkgs.linuxPackages.usbip}/bin/usbip list -p --remote 192.168.0.21 | grep 0458:6006 | grep -oE "[0-9]+-[0-9]+\.[0-9]+")'
       '';
       ExecStop = "${pkgs.linuxPackages.usbip}/bin/usbip detach --port 0";
+      Restart = "on-failure";
+      RestartSec = 60;
+      StartLimitIntervalSec = 0; # infinte retries
+      StartLimitBurst = 0; # infinte retries
       RemainAfterExit = true;
       User = "root";
     };
