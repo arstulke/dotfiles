77c95e564c2c96706c37434e9fc1c8b83ba3250c
Arne Stulken <21034491+arstulke@users.noreply.github.com>
2026-01-05 00:47:42 +0100

    general: adding nix-diff package and build alias

--- nix/modules/utils/lifecycle.nix
+++ nix/modules/utils/lifecycle.nix
@@ -6,7 +6,10 @@
       flake = flakePath;
   };
 
+  environment.systemPackages = with pkgs; [ nix-diff ];
+
   environment.shellAliases = {
+    build         = "nh os build -H $NIX_FLAKE_DEFAULT_HOST --impure -- --print-out-paths";
     rebuild       = "nh os switch -H $NIX_FLAKE_DEFAULT_HOST --impure";
     rebuild-test  = "nh os test -H $NIX_FLAKE_DEFAULT_HOST --impure";
     update        = "nix flake update --flake ${flakePath}";
