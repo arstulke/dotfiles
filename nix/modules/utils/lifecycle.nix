d46192bfd1f32428be37b0e9dcc6c2743904808a
Arne Stulken <21034491+arstulke@users.noreply.github.com>
2026-01-05 00:48:27 +0100

    general: disabling impure build-flag

--- nix/modules/utils/lifecycle.nix
+++ nix/modules/utils/lifecycle.nix
@@ -9,9 +9,9 @@
   environment.systemPackages = with pkgs; [ nix-diff ];
 
   environment.shellAliases = {
-    build         = "nh os build -H $NIX_FLAKE_DEFAULT_HOST --impure -- --print-out-paths";
-    rebuild       = "nh os switch -H $NIX_FLAKE_DEFAULT_HOST --impure";
-    rebuild-test  = "nh os test -H $NIX_FLAKE_DEFAULT_HOST --impure";
+    build         = "nh os build -H $NIX_FLAKE_DEFAULT_HOST -- --print-out-paths";
+    rebuild       = "nh os switch -H $NIX_FLAKE_DEFAULT_HOST";
+    rebuild-test  = "nh os test -H $NIX_FLAKE_DEFAULT_HOST";
     update        = "nix flake update --flake ${flakePath}";
     free-nix      = "sudo nix-collect-garbage --delete-older-than 30d";
   };
