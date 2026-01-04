dc89a404ac0b26031aa3aab5cee3cee5c9942839
Arne Stulken <21034491+arstulke@users.noreply.github.com>
2026-01-05 00:38:52 +0100

    general: keybinding for editing config

--- nix/modules/desktop/gnome/keybindings.nix
+++ nix/modules/desktop/gnome/keybindings.nix
@@ -6,6 +6,7 @@
       custom-keybindings = [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/my-open-terminal/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/my-open-filemanager/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/my-edit-config/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/my-flameshot/"
       ];
     };
@@ -20,6 +21,12 @@
       name = "Open file manager";
       command = "nautilus ./Downloads";
       binding = "<Super>e";
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/my-edit-config" = {
+      name = "Edit dotfile config";
+      command = "code /etc/dotfiles/nix"; # TODO generalize for CLI only and for GUI (maybe define global alias for opening editor)
+      binding = "<Super>c";
     };
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/my-flameshot" =
