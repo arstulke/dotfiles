c34ada05da5768ee68c2aa735f8c90cdac502474
Arne Stulken <21034491+arstulke@users.noreply.github.com>
2026-01-06 21:51:48 +0100

    feat(general audio): Adding shell alias for fixing audio bugs

--- nix/modules/hardware/audio.nix
+++ nix/modules/hardware/audio.nix
@@ -1,25 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Troubleshooting sound playback issues:
-  #   Problem 1:
-  #     Symptoms:
-  #       - GNOME Settings speaker test freezes
-  #       - `speaker-test -c 2` (from alsa-utils) hangs
-  #       - Spotify fails to play audio
-  #       - Browser audio playback crashes (e.g., YouTube)
-  #
-  #     Cause:
-  #       PipeWire / WirePlumber state corruption or deadlock
-  #
-  #     Solution:
-  #       Clear cache and restart pulseaudio and wireplumber:
-  #         ```
-  #         rm -rf ~/.local/state/wireplumber
-  #         rm -rf ~/.cache/wireplumber
-  #         systemctl --user restart pipewire wireplumber pipewire-pulse
-  #         ```
-
   security.rtkit.enable = true;
   services.pulseaudio.enable = false;
   services.pipewire = {
@@ -33,4 +14,21 @@
     pavucontrol
     helvum
   ];
+
+  # Troubleshooting sound playback issues:
+  #   Problem 1:
+  #     Symptoms:
+  #       - GNOME Settings speaker test freezes
+  #       - `speaker-test -c 2` (from alsa-utils) hangs
+  #       - Spotify fails to play audio
+  #       - Browser audio playback crashes (e.g., YouTube)
+  #
+  #     Cause:
+  #       PipeWire / WirePlumber state corruption or deadlock
+  #
+  #     Solution:
+  #       Clear cache and restart pulseaudio and wireplumber: `fix-audio`
+  environment.shellAliases = {
+    fix-audio = "rm -rf ~/.cache/wireplumber ~/.local/state/wireplumber/ && systemctl --user restart pipewire wireplumber pipewire-pulse";
+  };
 }
