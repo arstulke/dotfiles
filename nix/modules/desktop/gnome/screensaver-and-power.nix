cb895b612ec85e959c8491acb087762d81a3d1cd
Arne Stulken <21034491+arstulke@users.noreply.github.com>
2026-01-17 19:40:12 +0100

    fix(gnome): Disabling screensaver & lock screen on idle

--- nix/modules/desktop/gnome/screensaver-and-power.nix
+++ nix/modules/desktop/gnome/screensaver-and-power.nix
@@ -1,23 +1,25 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   hm.dconf.settings = {
     "org/gnome/settings-daemon/plugins/power" = {
       idle-dim = false;
-      sleep-inactive-battery-timeout = 900; # 15min
+      sleep-inactive-battery-timeout = 7200; # 2h
       sleep-inactive-battery-type = "nothing";
-      sleep-inactive-ac-timeout = 900; # 15min
+      sleep-inactive-ac-timeout = 7200; # 2h
       sleep-inactive-ac-type = "nothing";
       power-button-action = "interactive";
     };
     "org/gnome/desktop/session" = {
-      idle-delay = 300; # 5min
+      idle-delay = lib.gvariant.mkUint32 0; # disables 'Automatic Screen Blank'
     };
     "org/gnome/desktop/screensaver" = {
-      lock-enabled = true;
-      lock-delay = 0; # 0sec
+      idle-activation-enabled = false;
+      lock-enabled = false; # disables screen lock on screensaver
+      lock-delay = lib.gvariant.mkUint32 0; # disables screensaver
     };
   };
 }
