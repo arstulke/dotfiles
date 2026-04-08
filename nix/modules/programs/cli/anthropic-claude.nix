22923684d12e873b22bb5e1cac90ea513ea8a0a8
Arne Stulken <21034491+arstulke@users.noreply.github.com>
2026-04-08 10:26:16 +0200

    feat(anthropic-claude): Adding gnome extension for usage indicator

--- nix/modules/programs/cli/anthropic-claude.nix
+++ nix/modules/programs/cli/anthropic-claude.nix
@@ -3,9 +3,41 @@
   lib,
   inputs,
   ...
-}: {
+}: let
+  originalGnomeExt = pkgs.gnomeExtensions.claude-code-usage-indicator;
+  patchedGnomeExt = pkgs.runCommand "patched-claude-code-usage-indicator" {} ''
+    # Copy the entire package output
+    cp -r ${originalGnomeExt} $out
+    chmod -R +w $out
+
+    # Read the original JSON, modify it, write it back
+    file=share/gnome-shell/extensions/ccusage-indicator@lordvcs.github.io/metadata.json
+    cat ${originalGnomeExt}/$file \
+      | ${pkgs.jq}/bin/jq '."shell-version" += ["49", "50"]' \
+      > $out/$file
+  '';
+
+  ccUsageCommand = pkgs.writeShellScriptBin "ccusage" ''
+    PATH=$PATH:${pkgs.nodejs_24}/bin ${pkgs.nodejs_24}/bin/npx ccusage "$@"
+  '';
+in {
   environment.systemPackages = with pkgs; [
     unstable.claude-code
-    claude-monitor
+
+    # usage monitor
+    claude-monitor # in CLI
+    patchedGnomeExt # in gnome statusbar
   ];
+
+  # usage monitor in gnome statusbar
+  hm.dconf.settings = {
+    "org/gnome/shell" = {
+      enabled-extensions = with pkgs; [
+        gnomeExtensions.claude-code-usage-indicator.extensionUuid
+      ];
+    };
+    "org/gnome/shell/extensions/ccusage-indicator" = {
+      "ccusage-command" = "${ccUsageCommand}/bin/ccusage";
+    };
+  };
 }
