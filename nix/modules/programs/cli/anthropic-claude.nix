5ad34ee86b5e9f573512328f596cbe14c0dc30b7
Arne Stulken <21034491+arstulke@users.noreply.github.com>
2026-04-08 10:26:16 +0200

    feat(anthropic-claude): Adding gnome extension for usage indicator

--- nix/modules/programs/cli/anthropic-claude.nix
+++ nix/modules/programs/cli/anthropic-claude.nix
@@ -5,7 +5,22 @@
   ...
 }: {
   environment.systemPackages = with pkgs; [
+    # core module
     unstable.claude-code
+
+    # useful for cli-only setups
     claude-monitor
+
+    # useful for gnome desktops
+    gnomeExtensions.claude-code-usage-indicator
   ];
+
+  # useful for gnome desktops
+  hm.dconf.settings = {
+    "org/gnome/shell" = {
+      enabled-extensions = with pkgs; [
+        gnomeExtensions.claude-code-usage-indicator.extensionUuid
+      ];
+    };
+  };
 }
