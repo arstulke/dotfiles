1cf8cc1ca7cab7e88a187ef785d11d04e1e481b1
Arne Stulken <21034491+arstulke@users.noreply.github.com>
2026-04-12 20:57:33 +0200

    feat(vps-01): Adding ollama service

--- nix/hosts/vps-01/default.nix
+++ nix/hosts/vps-01/default.nix
@@ -16,6 +16,23 @@
     };
   };
 
+  # Ollama for local LLM inference
+  services.ollama = {
+    enable = true;
+    package = pkgs.unstable.ollama-cpu;
+    loadModels = [
+      "gemma4:e2b"
+    ];
+    host = "0.0.0.0"; # allows access from other devices on the network
+    environmentVariables = {
+      OLLAMA_NOHISTORY = "1";
+      OLLAMA_MAX_LOADED_MODELS = "1";
+      OLLAMA_KEEP_ALIVE = "30m";
+      OLLAMA_NUM_PARALLEL = "2";
+    };
+  };
+  environment.systemPackages = with pkgs; [unstable.llmfit];
+
   # Bootloader
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/vda";
